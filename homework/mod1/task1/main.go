d2799781e72ca5dbde284a6299c277ef39f783ad
iDeBugger Freeman <me@idebugger.io>
2020-03-15 10:01:42 -0500

    Fix task1

--- homework/mod1/task1/main.go
+++ homework/mod1/task1/main.go
@@ -1,15 +1,17 @@
 package main
 
-func ShiftSlice(s []int) []int {
-	for i := 0; i < len(s) - 1; i++ {
-		s[i], s[i + 1] = s[i + 1], s[i]
+func ShiftSlice(s []int, n int) []int {
+	for j := 0; j < n; j++ {
+		for i := 0; i < len(s)-1; i++ {
+			s[i], s[i+1] = s[i+1], s[i]
+		}
 	}
 	return s
 }
 
 func ReverseSlice(s []int) []int {
-	for i := 0; i < len(s) / 2; i++ {
-		s[i], s[len(s) - i - 1] = s[len(s) - i - 1], s[i]
+	for i := 0; i < len(s)/2; i++ {
+		s[i], s[len(s)-i-1] = s[len(s)-i-1], s[i]
 	}
 	return s
 }
--- homework/mod1/task1/main_test.go
+++ homework/mod1/task1/main_test.go
@@ -8,6 +8,7 @@ import (
 func TestShiftSlice(t *testing.T) {
 	type args struct {
 		s []int
+		n int
 	}
 	tests := []struct {
 		name string
@@ -16,28 +17,58 @@ func TestShiftSlice(t *testing.T) {
 	}{
 		{
 			name: "Empty slice",
-			args: args{s: []int{}},
+			args: args{s: []int{}, n: 1},
 			want: []int{},
 		},
 		{
-			name: "Slice with one element",
-			args: args{s: []int{12}},
+			name: "Shift for 1 on slice with one element",
+			args: args{s: []int{12}, n: 1},
 			want: []int{12},
 		},
 		{
-			name: "Slice with two elements",
-			args: args{s: []int{12, 22}},
+			name: "Shift for 1 on slice with two elements",
+			args: args{s: []int{12, 22}, n: 1},
 			want: []int{22, 12},
 		},
 		{
-			name: "Slice with several elements",
-			args: args{s: []int{12, 22, 33, 44}},
+			name: "Shift for 1 on slice with several elements",
+			args: args{s: []int{12, 22, 33, 44}, n: 1},
 			want: []int{22, 33, 44, 12},
+		},
+		{
+			name: "Shift for 0 on empty slice",
+			args: args{s: []int{}, n: 0},
+			want: []int{},
+		},
+		{
+			name: "Shift for 0 on non-empty slice",
+			args: args{s: []int{1, 2, 3, 4}, n: 0},
+			want: []int{1, 2, 3, 4},
+		},
+		{
+			name: "Shift for 2 on empty slice",
+			args: args{s: []int{}, n: 2},
+			want: []int{},
+		},
+		{
+			name: "Shift for 2 on slice with one element",
+			args: args{s: []int{12}, n: 2},
+			want: []int{12},
+		},
+		{
+			name: "Shift for 2 on slice with two elements",
+			args: args{s: []int{12, 22}, n: 2},
+			want: []int{12, 22},
+		},
+		{
+			name: "Shift for 2 on slice with several elements",
+			args: args{s: []int{12, 22, 33, 44}, n: 2},
+			want: []int{33, 44, 12, 22},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := ShiftSlice(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+			if got := ShiftSlice(tt.args.s, tt.args.n); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("ShiftSlice() = %v, want %v", got, tt.want)
 			}
 		})
